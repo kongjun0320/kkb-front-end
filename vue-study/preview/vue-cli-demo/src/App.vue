9bb0ff2b067eaa640c6e06508e99d35b7d4ef78b
kongjun0320 <1741142595@qq.com>
2021-04-17 09:30:24 +0800

    使用vue-cli重构购物车

--- vue-study/preview/vue-cli-demo/src/App.vue
+++ vue-study/preview/vue-cli-demo/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
     <h1>购物车</h1>
+    <course-add @add-course="_addCourse" />
     <course-list :courses="courses" />
   </div>
 </template>
 
 <script>
 import CourseList from './components/CourseList.vue'
+import CourseAdd from './components/CourseAdd.vue'
 import { getCourses } from './api'
 
 export default {
-  components: { CourseList },
+  components: { CourseList, CourseAdd },
   name: 'App',
   data() {
     return {
@@ -19,6 +21,11 @@ export default {
   },
   async created() {
     this.courses = await getCourses()
+  },
+  methods: {
+    _addCourse(course) {
+      this.courses.push({ name: course })
+    }
   }
 }
 </script>
